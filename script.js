a02f9285c8516e991b9902655079f034ee994e66
darinazhuirikova <166602422+darinazhuirikova@users.noreply.github.com>
2024-05-04 15:11:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ function checkIngredient() {
     var ingredientInput = document.getElementById("ingredientInput");
     var ingredient = ingredientInput.value.trim().toLowerCase();
 
+
     var validIngredients = ["flour", "sugar", "egg", "butter", "milk", "cream"];
-    
+   
     if (validIngredients.includes(ingredient)) {
         document.getElementById("result").innerText = "This ingridient suits for your cake!";
     } else {
         document.getElementById("result").innerText = "This ingridient doesn't suit for your cake, Try again!";
     }
 }
+
+
 
 
 counter = 0;
@@ -65,6 +68,4 @@ function cook(){
 function bakeCake() {
     document.getElementById('result').style.display = 'block';
 }
-
-
 
