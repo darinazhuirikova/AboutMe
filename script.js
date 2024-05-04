716377e85dc8442e7b19efea10ba1e6eabe302c6
darinazhuirikova <166602422+darinazhuirikova@users.noreply.github.com>
2024-05-04 15:02:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,59 @@ function checkIngredient() {
 }
 
 
+counter = 0;
+function cook(){
+    let h1 = document.getElementById("restit")
+    let cookInput = document.getElementById("cookInput")
+    if(counter == 0){
+        counter++;
+ 
+        let text = cookInput.value
+ 
+        if(text == "мука"){
+            h1.innerHTML = "мука есть, что дальше?"
+            if(text == "яйцо"){
+                h1.innerHTML = "яйцо есть, что дальше?"
+                if(text == "молоко"){
+                    h1.innerHTML = "молоко есть, что дальше?"
+                    if(text == "масло"){
+                        h1.innerHTML = "масло есть, что дальше?"
+                        if(text == "корсахар"){
+                            h1.innerHTML = "корсахар есть, что дальше?"
+                            if(text == "выпекать"){
+                                h1.innerHTML = "молодец, нажми кнопку снизу!"
+                            }
+                            else {
+                                h1.innerHTML = "не правильно, посмотри рецепт!"
+                            }
+                        }
+                        else {
+                            h1.innerHTML = "не правильно, посмотри рецепт!"
+                        }
+                    }
+                    else {
+                        h1.innerHTML = "не правильно, посмотри рецепт!"
+                    }
+                }
+                else {
+                    h1.innerHTML = "не правильно, посмотри рецепт!"
+                }
+            }
+            else {
+                h1.innerHTML = "не правильно, посмотри рецепт!"
+            }
+        }
+        else {
+            h1.innerHTML = "не правильно, посмотри рецепт!"
+        }
+    }
+    if(counter == 1){
+         
+    }
+}
+function bakeCake() {
+    document.getElementById('result').style.display = 'block';
+}
+
+
 
