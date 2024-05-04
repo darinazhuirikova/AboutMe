b78296ed0e5446624c35034bd127c0864d569382
darinazhuirikova <166602422+darinazhuirikova@users.noreply.github.com>
2024-05-05 00:56:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ function checkIngredient() {
     var ingredientInput = document.getElementById("ingredientInput");
     var ingredient = ingredientInput.value.trim().toLowerCase();
 
-
     var validIngredients = ["flour", "sugar", "egg", "butter", "milk", "cream"];
-   
+    
     if (validIngredients.includes(ingredient)) {
         document.getElementById("result").innerText = "This ingridient suits for your cake!";
     } else {
@@ -13,53 +12,89 @@ function checkIngredient() {
 }
 
 
-
-
-counter = 0;
-function cook(){
-    let h1 = document.getElementById("restit")
-    let cookInput = document.getElementById("cookInput")
+counter = 0; 
+function cook(){ 
+    let h9 = document.getElementById("restit") 
+    let cookInput = document.getElementById("cookInput") 
  
-        let text = cookInput.value
+        let text = cookInput.value 
  
         if(text == "мука"){
-            h1.innerHTML = "мука есть, что дальше?"
+            h9.innerHTML = "мука есть, что дальше?" 
             if(text == "яйцо"){
-                h1.innerHTML = "яйцо есть, что дальше?"
+                h9.innerHTML = "яйцо есть, что дальше?" 
                 if(text == "молоко"){
-                    h1.innerHTML = "молоко есть, что дальше?"
+                    h9.innerHTML = "молоко есть, что дальше?" 
                     if(text == "масло"){
-                        h1.innerHTML = "масло есть, что дальше?"
+                        h9.innerHTML = "масло есть, что дальше?" 
                         if(text == "корсахар"){
-                            h1.innerHTML = "корсахар есть, что дальше?"
+                            h9.innerHTML = "корсахар есть, что дальше?" 
                             if(text == "выпекать"){
-                                h1.innerHTML = "молодец, нажми кнопку снизу!"
+                                h9.innerHTML = "молодец, нажми кнопку снизу!" 
+                                btn2.classList.remove("hidden")
                             }
                             else {
-                                h1.innerHTML = "не правильно, посмотри рецепт!"
+                                h9.innerHTML = "не правильно, посмотри рецепт!" 
                             }
                         }
                         else {
-                            h1.innerHTML = "не правильно, посмотри рецепт!"
+                            h9.innerHTML = "не правильно, посмотри рецепт!" 
                         }
                     }
                     else {
-                        h1.innerHTML = "не правильно, посмотри рецепт!"
+                        h9.innerHTML = "не правильно, посмотри рецепт!" 
                     }
                 }
                 else {
-                    h1.innerHTML = "не правильно, посмотри рецепт!"
+                    h9.innerHTML = "не правильно, посмотри рецепт!" 
                 }
             }
             else {
-                h1.innerHTML = "не правильно, посмотри рецепт!"
+                h9.innerHTML = "не правильно, посмотри рецепт!" 
             }
         }
         else {
-            h1.innerHTML = "не правильно, посмотри рецепт!"
+            h9.innerHTML = "не правильно, посмотри рецепт!" 
         }
-    }
+    } 
 function bakeCake() {
-    document.getElementById('result').style.display = 'block';
+    if(counter == 0){ 
+        counter++; 
+        h9.innerHTML = "молодцы, вы сделали торт!" 
+    } 
+    if(counter == 1){ 
+         
+    } 
 }
 
+function Insta() {
+    let a = [ 
+        "https://img.freepik.com/free-vector/hand-drawn-cartoon-ant-illustration_23-2150362250.jpg", 
+        "https://cdn2.vectorstock.com/i/1000x1000/79/61/cartoon-of-ants-colony-on-the-tree-vector-39727961.jpg", 
+        "https://cdn5.vectorstock.com/i/1000x1000/99/34/funny-black-ant-climbing-a-tree-with-rope-cartoon-vector-27229934.jpg", 
+    ];
+
+    let texts = [
+        "Первый текст",
+        "Второй текст",
+        "Третий текст"
+    ];
+
+    let gameBox = document.getElementById("gameBox");
+
+    for(let i = 0; i < 100; i++) {
+        let newImg = document.createElement("img");
+        newImg.setAttribute("src", a[i]);
+
+        let textDiv = document.createElement("div");
+        textDiv.textContent = texts[i];
+        textDiv.classList.add("littletext7")
+
+        let containerDiv = document.createElement("div");
+        containerDiv.classList.add("image-container");
+        containerDiv.appendChild(newImg);
+        containerDiv.appendChild(textDiv);
+
+        gameBox.appendChild(containerDiv);
+    }
+}
