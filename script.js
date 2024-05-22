dcd315bbe99f86479c967e9fd47750b4ab2f3aa1
darinazhuirikova <166602422+darinazhuirikova@users.noreply.github.com>
2024-05-22 15:59:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -67,34 +67,3 @@ function bakeCake() {
     } 
 }
 
-function Insta() {
-    let a = [ 
-        "https://img.freepik.com/free-vector/hand-drawn-cartoon-ant-illustration_23-2150362250.jpg", 
-        "https://cdn2.vectorstock.com/i/1000x1000/79/61/cartoon-of-ants-colony-on-the-tree-vector-39727961.jpg", 
-        "https://cdn5.vectorstock.com/i/1000x1000/99/34/funny-black-ant-climbing-a-tree-with-rope-cartoon-vector-27229934.jpg", 
-    ];
-
-    let texts = [
-        "Первый текст",
-        "Второй текст",
-        "Третий текст"
-    ];
-
-    let gameBox = document.getElementById("gameBox");
-
-    for(let i = 0; i < 100; i++) {
-        let newImg = document.createElement("img");
-        newImg.setAttribute("src", a[i]);
-
-        let textDiv = document.createElement("div");
-        textDiv.textContent = texts[i];
-        textDiv.classList.add("littletext7")
-
-        let containerDiv = document.createElement("div");
-        containerDiv.classList.add("image-container");
-        containerDiv.appendChild(newImg);
-        containerDiv.appendChild(textDiv);
-
-        gameBox.appendChild(containerDiv);
-    }
-}
